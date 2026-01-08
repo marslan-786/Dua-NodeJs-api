3c4d042d9d25dcf78f4346666eedaf6c25652402
marslan-786 <marslansalfias@gmail.com>
2026-01-08 17:08:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const BASE_URL = "http://51.89.99.105/NumberPanel";
 const STATS_PAGE_URL = `${BASE_URL}/client/SMSCDRStats`; 
 
 const COMMON_HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Mobile Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 13; V2040 Build/TP1A.220624.014) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.7499.34 Mobile Safari/537.36",
     "X-Requested-With": "XMLHttpRequest",
     "Origin": "http://51.89.99.105",
     "Accept-Language": "en-US,en;q=0.9,ur-PK;q=0.8,ur;q=0.7"
@@ -161,7 +161,7 @@ app.get('/api', async (req, res) => {
         // Client SMS URL (Using extracted SessKey)
         // Notice: iColumns=7 for Client (Agent had 9)
         specificReferer = `${BASE_URL}/client/SMSCDRStats`;
-        targetUrl = `${BASE_URL}/client/res/data_smscdr.php?fdate1=2025-12-17%2000:00:00&fdate2=2259-12-20%2023:59:59&frange=&fnum=&fcli=&fgdate=&fgmonth=&fgrange=&fgnumber=&fgcli=&fg=0&sesskey=${STATE.sessKey}&sEcho=2&iColumns=7&sColumns=%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=-1&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=true&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=desc&iSortingCols=1&_=${ts}`;
+        targetUrl = `${BASE_URL}/client/res/data_smscdr.php?fdate1=2026-01-07%2000:00:00&fdate2=2259-12-20%2023:59:59&frange=&fnum=&fcli=&fgdate=&fgmonth=&fgrange=&fgnumber=&fgcli=&fg=0&sesskey=${STATE.sessKey}&sEcho=2&iColumns=7&sColumns=%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=-1&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=true&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=desc&iSortingCols=1&_=${ts}`;
     
     } else {
         return res.status(400).json({ error: "Invalid type. Use ?type=sms or ?type=numbers" });
