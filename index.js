c3f3f295fa7f18542029690283f5a421605c1c23
marslan-786 <marslansalfias@gmail.com>
2026-02-02 21:09:20 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 5000;
 
 // --- CONFIGURATION (CLIENT) ---
 const CREDENTIALS = {
-    username: "Kami527", // Client Username
-    password: "Kami526"  // Client Password
+    username: "Alizashaikh01", // Client Username
+    password: "Alizashaikh01"  // Client Password
 };
 
 const BASE_URL = "http://51.89.99.105/NumberPanel";
